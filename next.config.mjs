1b7cb56fd247c4153d47c883f8f777d74c18513e
Purvansh Joshi <purvanshjoshi7534011576@gmail.com>
2026-06-16 21:08:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js dynamic image optimization
+  },
+  // This tells Next.js to prefix all asset paths with your repository name
+  basePath: '/IEEESoC.web',
+  assetPrefix: '/IEEESoC.web/',
 };
 
 export default nextConfig;
